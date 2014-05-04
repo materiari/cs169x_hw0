0de9bd6f952cb55aa4f99724ae1f4da045d03429
Aris Berard <aris@shore.net>
2014-05-04 14:10:06 -0700

    HW0.1 as submitted to grader.

--- hw0.1.rb
+++ hw0.1.rb
@@ -1,13 +1,16 @@
+# Sum of numbers in an array of integers
 def sum(int_array)
   my_sum = 0
   int_array.each {|x| my_sum += x}
   my_sum
 end
 
+# Sum of the two greatest integers in the array
 def max_2_sum(int_array)
   sum(int_array.sort {|a,b| b <=> a}[0,2])
 end
 
+# Predicate for any two elements adding up to the given value
 def sum_to_n?(int_array, n)
   int_array.uniq.combination(2).detect {|arr| sum(arr) == n }
 end
---
+++ test_hw_0.1.rb
@@ -0,0 +1,5 @@
+require './hw0.1.rb'
+
+puts "sum([3,2,1] #{sum([3,2,1])}"
+puts "sum([]) #{sum([])}"
+
