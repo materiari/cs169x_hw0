925f12b7532139024e4473cf32f40141635c1161
OpenShift guest <aris@shore.net>
2014-05-05 21:37:26 -0400

    Add more tests and fix filename inconsistency.

--- test_hw_0.1.rb
+++ test_hw_0.1.rb
@@ -1,5 +1,15 @@
-require './hw0.1.rb'
+require './hw_0.1.rb'
 
 puts "sum([3,2,1] #{sum([3,2,1])}"
 puts "sum([]) #{sum([])}"
+
+puts "max_2_sum([5,4,1,9,12]) #{max_2_sum([5,4,1,9,12])}"
+puts "max_2_sum([]) #{max_2_sum([])}"
+puts "max_2_sum([5,4,1,-9,12]) #{max_2_sum([5,4,1,-9,12])}"
+
+puts "sum_to_n? [5,4,1,9,12], 9 #{sum_to_n? [5,4,1,9,12], 9}"
+puts "sum_to_n? [5,4,1,9,12], 7 #{sum_to_n? [5,4,1,9,12], 7}"
+puts "sum_to_n? [7], 7 #{sum_to_n? [7], 7}"
+puts "sum_to_n? [], 7 #{sum_to_n? [], 7}"
+
 
